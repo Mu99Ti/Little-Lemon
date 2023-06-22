6479ff733d2e74e2e50b8c22186aeff4714b474f
Muhamad Talebi <muhamad79t@gmail.com>
2023-06-22 08:30:44 +0330

    Initial Commit

---
+++ Little-Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little-Lemon
+//
+//  Created by Muhamad Talebi on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
